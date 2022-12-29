4d28c0196839e983f7569a6212fb264498d8f90c
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2022-12-29 17:46:43 +0200

    usePointerLock fix

--- resources/js/composables/usePointerLock.ts
+++ resources/js/composables/usePointerLock.ts
@@ -1,47 +1,56 @@
 import { ref } from "vue";
-import { createSharedComposable, until } from "@vueuse/shared";
+import { until } from "@vueuse/shared";
 import { useEventListener, useSupported, unrefElement } from "@vueuse/core";
-import type { MaybeElement, MaybeElementRef } from "@vueuse/core";
+import type { MaybeElementRef } from "@vueuse/core";
 import type { ConfigurableDocument } from "@vueuse/core";
 import { defaultDocument } from "@vueuse/core";
 
-const useSharedEventListener = createSharedComposable(useEventListener);
+type MaybeHTMLElement = HTMLElement | undefined | null;
 
 /**
  * Reactive pointer lock.
  */
-export function usePointerLock(target?: MaybeElementRef, options: ConfigurableDocument = {}) {
+export function usePointerLock(target?: MaybeElementRef<MaybeHTMLElement>, options: ConfigurableDocument = {}) {
     const { document = defaultDocument } = options;
 
     const isSupported = useSupported(() => document && "pointerLockElement" in document);
 
-    const element = ref<MaybeElement>();
+    const element = ref<MaybeHTMLElement>();
+
+    let pendingElement: HTMLElement;
 
     if (isSupported.value) {
-        useSharedEventListener(document, "pointerlockchange", () => {
-            console.log(document!.pointerLockElement ? "locked" : "unlocked");
-            element.value = document!.pointerLockElement as MaybeElement;
+        useEventListener(document, "pointerlockchange", () => {
+            const currentElement = document!.pointerLockElement ?? element.value;
+            if (pendingElement && currentElement === pendingElement) {
+                console.log(document!.pointerLockElement ? "locked" : "unlocked");
+                element.value = document!.pointerLockElement as MaybeHTMLElement;
+            }
         });
 
-        useSharedEventListener(document, "pointerlockerror", () => {
+        useEventListener(document, "pointerlockerror", () => {
             console.log("error");
             const action = document!.pointerLockElement ? "release" : "acquire";
             throw new Error(`Failed to ${action} pointer lock.`);
         });
     }
 
-    async function lock(e: MaybeElementRef | Event) {
+    async function lock(e: MaybeElementRef<MaybeHTMLElement> | Event) {
         if (!isSupported.value) throw new Error("Pointer Lock API is not supported by your browser.");
 
-        const targetElement = e instanceof Event ? unrefElement(target) ?? <Element>e.target : unrefElement(e);
+        const targetElement = e instanceof Event ? unrefElement(target) ?? <HTMLElement>e.target : unrefElement(e);
         if (!targetElement) throw new Error("Target element undefined.");
         targetElement.requestPointerLock();
+        pendingElement = targetElement;
 
         return await until(element).toBe(targetElement);
     }
 
     async function unlock() {
-        element.value && document!.exitPointerLock();
+        if (element.value) {
+            document!.exitPointerLock();
+            pendingElement = element.value;
+        }
 
         return await until(element).toBe(null);
     }
