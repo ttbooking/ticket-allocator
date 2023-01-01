eee9efbfaf140073613ba0628c2d9f0aa4ee6f54
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-01-01 23:58:30 +0200

    usePointerLock fix

--- resources/js/composables/usePointerLock.ts
+++ resources/js/composables/usePointerLock.ts
@@ -17,42 +17,41 @@ export function usePointerLock(target?: MaybeElementRef<MaybeHTMLElement>, optio
 
     const element = ref<MaybeHTMLElement>();
 
-    let pendingElement: HTMLElement;
+    let targetElement: MaybeHTMLElement;
 
     if (isSupported.value) {
         useEventListener(document, "pointerlockchange", () => {
             const currentElement = document!.pointerLockElement ?? element.value;
-            if (pendingElement && currentElement === pendingElement) {
-                console.log(document!.pointerLockElement ? "locked" : "unlocked");
+            if (targetElement && currentElement === targetElement)
                 element.value = document!.pointerLockElement as MaybeHTMLElement;
-            }
         });
 
         useEventListener(document, "pointerlockerror", () => {
-            console.log("error");
-            const action = document!.pointerLockElement ? "release" : "acquire";
-            throw new Error(`Failed to ${action} pointer lock.`);
+            const currentElement = document!.pointerLockElement ?? element.value;
+            if (targetElement && currentElement === targetElement) {
+                const action = document!.pointerLockElement ? "release" : "acquire";
+                throw new Error(`Failed to ${action} pointer lock.`);
+            }
         });
     }
 
     async function lock(e: MaybeElementRef<MaybeHTMLElement> | Event) {
         if (!isSupported.value) throw new Error("Pointer Lock API is not supported by your browser.");
 
-        const targetElement = e instanceof Event ? unrefElement(target) ?? <HTMLElement>e.target : unrefElement(e);
+        targetElement = e instanceof Event ? unrefElement(target) ?? <HTMLElement>e.currentTarget : unrefElement(e);
         if (!targetElement) throw new Error("Target element undefined.");
         targetElement.requestPointerLock();
-        pendingElement = targetElement;
 
         return await until(element).toBe(targetElement);
     }
 
     async function unlock() {
-        if (element.value) {
-            document!.exitPointerLock();
-            pendingElement = element.value;
-        }
+        if (!element.value) return false;
 
-        return await until(element).toBe(null);
+        document!.exitPointerLock();
+
+        targetElement = await until(element).toBeNull();
+        return true;
     }
 
     return {
