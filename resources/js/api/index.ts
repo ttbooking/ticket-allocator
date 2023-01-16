af036f37e48a0baf7db359f87ec97034378ca8fb
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-01-16 14:37:29 +0200

    Fix SupervisorController

--- resources/js/api/index.ts
+++ resources/js/api/index.ts
@@ -2,15 +2,19 @@ import route from "ziggy-js";
 
 export function useSupervisorApi() {
     async function ready(operatorUuid: string, ready: boolean) {
-        return await window.axios.patch(route("ticket-allocator.api.ready", operatorUuid), ready);
+        return await window.axios.patch(route("ticket-allocator.api.ready", operatorUuid), { ready });
     }
 
     async function weight(ticketUuid: string, weightPoints: number) {
-        return await window.axios.patch(route("ticket-allocator.api.weight", ticketUuid), weightPoints);
+        return await window.axios.patch(route("ticket-allocator.api.weight", ticketUuid), {
+            weight_points: weightPoints,
+        });
     }
 
-    async function handler(ticketUuid: string, operatorUuid?: string) {
-        return await window.axios.patch(route("ticket-allocator.api.handler", ticketUuid), operatorUuid);
+    async function handler(ticketUuid: string, operatorUuid?: string | null) {
+        return await window.axios.patch(route("ticket-allocator.api.handler", ticketUuid), {
+            operator_uuid: operatorUuid ?? null,
+        });
     }
 
     async function close(ticketUuid: string) {
--- src/Http/Controllers/SupervisorController.php
+++ src/Http/Controllers/SupervisorController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace TTBooking\TicketAllocator\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Response;
 use TTBooking\TicketAllocator\Domain\Operator\Actions\SetOperatorNotReadyAction;
@@ -19,11 +20,13 @@ class SupervisorController extends Controller
      * Set operator readiness status.
      *
      * @param  Operator  $operator
-     * @param  bool  $ready
+     * @param  Request  $request
      * @return JsonResponse
      */
-    public function ready(Operator $operator, bool $ready): JsonResponse
+    public function ready(Operator $operator, Request $request): JsonResponse
     {
+        $ready = $request->ready;
+
         $ready
             ? app(SetOperatorReadyAction::class)($operator)
             : app(SetOperatorNotReadyAction::class)($operator);
@@ -35,11 +38,13 @@ class SupervisorController extends Controller
      * Adjust ticket weight.
      *
      * @param  Ticket  $ticket
-     * @param  int  $weightPoints
+     * @param  Request  $request
      * @return JsonResponse
      */
-    public function weight(Ticket $ticket, int $weightPoints): JsonResponse
+    public function weight(Ticket $ticket, Request $request): JsonResponse
     {
+        $weightPoints = $request->weight_points;
+
         if ($weightPoints > 0) {
             app(Actions\IncrementTicketInitialWeightAction::class)($ticket, $weightPoints);
         } elseif ($weightPoints < 0) {
@@ -53,11 +58,13 @@ class SupervisorController extends Controller
      * Bind or unbind ticket.
      *
      * @param  Ticket  $ticket
-     * @param  Operator|null  $handler
+     * @param  Request  $request
      * @return JsonResponse
      */
-    public function handler(Ticket $ticket, ?Operator $handler): JsonResponse
+    public function handler(Ticket $ticket, Request $request): JsonResponse
     {
+        $handler = Operator::find($request->operator_uuid);
+
         if ($handler) {
             app(Actions\BindTicketAction::class)($ticket, $handler);
         } else {
