e9810626413a746e03fc095a768c31998d4fa3e2
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-01-16 23:49:01 +0200

    useDragAndDrop: default drag handler

--- resources/js/composables/useDragAndDrop.ts
+++ resources/js/composables/useDragAndDrop.ts
@@ -2,17 +2,25 @@ import { ref } from "vue";
 import type { MaybeComputedRef } from "@vueuse/core";
 import { useEventListener } from "@vueuse/core";
 
-export type DragStartHandler = (dataTransfer: DataTransfer | null) => void;
+export type DragStartHandler = (element: HTMLElement, dataTransfer: DataTransfer | null) => void;
 
 export function useDragAndDrop(
     target: MaybeComputedRef<HTMLElement | null | undefined>,
-    onDragStart?: DragStartHandler
+    onDragStart?: DragStartHandler | string
 ) {
     const element = ref<HTMLElement | null | undefined>();
 
     useEventListener<DragEvent>(target, "dragstart", (event) => {
         element.value = event.target as HTMLElement;
-        onDragStart?.(event.dataTransfer);
+
+        if (typeof onDragStart === "string") {
+            const entry = onDragStart;
+            onDragStart = (element, dataTransfer) => {
+                dataTransfer?.setData("text/plain", element.dataset[entry] ?? "");
+            };
+        }
+
+        onDragStart?.(element.value, event.dataTransfer);
     });
 
     useEventListener<DragEvent>(target, "dragend", () => {
