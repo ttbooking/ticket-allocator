05e3cb0bb5ced9630eda3563a89e2bbb43407ab7
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-03-01 15:08:05 +0200

    Operator team create/edit form converted to grid

--- resources/js/Pages/OperatorTeam/CreateEdit.vue
+++ resources/js/Pages/OperatorTeam/CreateEdit.vue
@@ -8,42 +8,67 @@
 
         <div>
             <v-form @submit.prevent="submit">
-                <v-checkbox v-model="form.active" label="Active" />
-                <v-text-field v-model="form.name" label="Name" :error-messages="errors.name" class="mb-4" />
-                <v-textarea
-                    v-model="form.description"
-                    label="Description"
-                    :error-messages="errors.description"
-                    class="mb-4"
-                />
-                <v-autocomplete
-                    v-model="form.operators"
-                    multiple
-                    clearable
-                    chips
-                    closable-chips
-                    label="Operators"
-                    :items="operators"
-                    item-title="name"
-                    item-value="uuid"
-                    :error-messages="errors.operators"
-                    class="mb-4"
-                />
-                <v-autocomplete
-                    v-model="form.ticket_categories"
-                    multiple
-                    clearable
-                    chips
-                    closable-chips
-                    label="Ticket categories"
-                    :items="ticketCategories"
-                    item-title="name"
-                    item-value="uuid"
-                    :error-messages="errors.ticket_categories"
-                    class="mb-4"
-                />
-                <v-btn type="submit" color="primary" class="mr-3" :disabled="form.processing">Save</v-btn>
-                <Link :href="route('ticket-allocator.teams.index')" class="mr-3"><v-btn>Cancel</v-btn></Link>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-checkbox v-model="form.active" label="Active" />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-text-field v-model="form.name" label="Name" :error-messages="errors.name" />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-textarea
+                                v-model="form.description"
+                                label="Description"
+                                :error-messages="errors.description"
+                            />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-autocomplete
+                                v-model="form.operators"
+                                multiple
+                                clearable
+                                chips
+                                closable-chips
+                                label="Operators"
+                                :items="operators"
+                                item-title="name"
+                                item-value="uuid"
+                                :error-messages="errors.operators"
+                            />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-autocomplete
+                                v-model="form.ticket_categories"
+                                multiple
+                                clearable
+                                chips
+                                closable-chips
+                                label="Ticket categories"
+                                :items="ticketCategories"
+                                item-title="name"
+                                item-value="uuid"
+                                :error-messages="errors.ticket_categories"
+                            />
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-btn type="submit" color="primary" class="mr-3" :disabled="form.processing">Save</v-btn>
+                            <Link :href="route('ticket-allocator.teams.index')" class="mr-3">
+                                <v-btn>Cancel</v-btn>
+                            </Link>
+                        </v-col>
+                    </v-row>
+                </v-container>
             </v-form>
         </div>
     </DefaultLayout>
