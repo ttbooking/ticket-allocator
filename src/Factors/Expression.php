e084a520da9981b7dc32587f76ef83a57eee1e3b
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-05-12 17:15:59 +0200

    Expressive factor evaluation

--- src/Factors/Expression.php
+++ src/Factors/Expression.php
@@ -4,19 +4,36 @@ declare(strict_types=1);
 
 namespace TTBooking\TicketAllocator\Factors;
 
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 use TTBooking\TicketAllocator\Attributes\Factors\Component;
 use TTBooking\TicketAllocator\Domain\Ticket\TicketAggregateRoot;
 
 #[Component('Factor/Partials/ExpressionForm')]
 class Expression extends Factor
 {
+    public function __construct(protected ExpressionLanguage $expression)
+    {
+    }
+
     public function getAdjustments(TicketAggregateRoot $ticket): array
     {
-        return [
-            'initial_weight' => 0,
-            'weight_increment' => 0,
-            'complexity' => 0,
-            'delay' => 0,
-        ];
+        $eval = function (string $metric) use ($ticket) {
+            $config = $this->getConfig();
+            $variables = $config['variables'] ?? [];
+            $expression = $config['expressions'][$metric] ?? null;
+            $adjustment = 0;
+
+            if ($expression) {
+                $adjustment = $this->expression->evaluate($expression, compact('ticket', 'variables'));
+
+                if (! is_int($adjustment)) {
+                    throw new \UnexpectedValueException('Expression evaluated to a non-integer value.');
+                }
+            }
+
+            return $adjustment;
+        };
+
+        return array_map($eval, ['initial_weight', 'weight_increment', 'complexity', 'delay']);
     }
 }
