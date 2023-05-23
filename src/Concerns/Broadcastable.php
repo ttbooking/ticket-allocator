30b94621b91372a83647d86bd1ed002688e9c6ab
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-05-23 12:48:05 +0200

    Common trait for ES and regular events

---
+++ src/Concerns/Broadcastable.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TTBooking\TicketAllocator\Concerns;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionProperty;
+
+trait Broadcastable
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public string $queue = 'event-sourcing';
+
+    protected string $channel = 'ticket-allocator';
+
+    protected ?string $name = null;
+
+    public function __sleep()
+    {
+        unset($this->queue);
+        unset($this->socket);
+
+        return array_keys($this->broadcastWith());
+    }
+
+    /**
+     * Get the tags that should be assigned to the job.
+     *
+     * @return string[]
+     */
+    public function tags(): array
+    {
+        return [$this->channel];
+    }
+
+    public function broadcastAs(): string
+    {
+        return $this->name ??= Str::kebab(class_basename(static::class));
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel($this->channel);
+    }
+
+    public function broadcastWith(): array
+    {
+        return collect((new ReflectionClass($this))->getProperties(ReflectionProperty::IS_PUBLIC))
+            ->reject(fn (ReflectionProperty $property) => property_exists(__CLASS__, $property->name))
+            ->mapWithKeys(fn (ReflectionProperty $property) => [$property->name => $property->getValue($this)])
+            ->all();
+    }
+}
--- src/Domain/Support/Event.php
+++ src/Domain/Support/Event.php
@@ -4,35 +4,18 @@ declare(strict_types=1);
 
 namespace TTBooking\TicketAllocator\Domain\Support;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Str;
-use ReflectionClass;
-use ReflectionProperty;
 use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
+use TTBooking\TicketAllocator\Concerns\Broadcastable;
 
 abstract class Event extends ShouldBeStored implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public string $queue = 'event-sourcing';
-
-    protected string $channel = 'ticket-allocator';
+    use Broadcastable;
 
     protected ?string $aggregateRootName = null;
 
     protected ?string $eventName = null;
-
-    public function __sleep()
-    {
-        unset($this->queue);
-        unset($this->socket);
-
-        return array_keys($this->broadcastWith());
-    }
 
     /**
      * Get the tags that should be assigned to the job.
@@ -47,19 +30,6 @@ abstract class Event extends ShouldBeStored implements ShouldBroadcast
     public function broadcastAs(): string
     {
         return $this->aggregateRootName().'.'.$this->eventName();
-    }
-
-    public function broadcastOn()
-    {
-        return new Channel($this->channel);
-    }
-
-    public function broadcastWith(): array
-    {
-        return collect((new ReflectionClass($this))->getProperties(ReflectionProperty::IS_PUBLIC))
-            ->reject(fn (ReflectionProperty $property) => property_exists(__CLASS__, $property->name))
-            ->mapWithKeys(fn (ReflectionProperty $property) => [$property->name => $property->getValue($this)])
-            ->all();
     }
 
     protected function aggregateRootName(): string
