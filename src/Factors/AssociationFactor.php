ef0024e42b6e375338b0c73f00d756b111f59393
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-05-25 11:49:25 +0200

    Abstract association factor

---
+++ src/Factors/AssociationFactor.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TTBooking\TicketAllocator\Factors;
+
+/**
+ * @template TEntry of array{
+ *     value: string,
+ *     initial_weight: int,
+ *     weight_increment: int,
+ *     complexity: int,
+ *     delay: int,
+ * }
+ *
+ * @extends Factor<TEntry[]>
+ */
+#[Attributes\Singular]
+#[Attributes\Component('Factor/Partials/AssociationForm')]
+abstract class AssociationFactor extends Factor
+{
+}
