2e09a0b76ebba39275ee607bcd3f985ff765b4c5
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-05-26 21:55:54 +0200

    TicketMetrics as simple DTO

--- src/DTO/TicketMetrics.php
+++ src/DTO/TicketMetrics.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace TTBooking\TicketAllocator\DTO;
 
-use Spatie\LaravelData\Data;
-
-class TicketMetrics extends Data
+class TicketMetrics
 {
     public function __construct(
-
         public int $initial_weight = 0,
-
         public int $weight_increment = 0,
-
         public int $complexity = 0,
-
         public int $delay = 0,
-
     ) {
+    }
+
+    public static function from(array $metrics): static
+    {
+        return new static(...$metrics);
     }
 }
