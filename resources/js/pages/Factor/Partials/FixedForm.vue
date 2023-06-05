bbdcbdeb47bb9e1a39fda2b513f0fcd220336dc7
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-06-05 11:47:58 +0200

    Minor fix

--- resources/js/pages/Factor/Partials/FixedForm.vue
+++ resources/js/pages/Factor/Partials/FixedForm.vue
@@ -1,24 +1,16 @@
 <template>
     <v-row>
         <v-col cols="3">
-            <v-text-field
-                v-model="config.initial_weight"
-                :label="$t('initial_weight')"
-                :error-messages="errors.initial_weight"
-            />
+            <v-text-field v-model="config.initial_weight" :label="$t('initial_weight')" />
         </v-col>
         <v-col cols="3">
-            <v-text-field
-                v-model="config.weight_increment"
-                :label="$t('weight_increment')"
-                :error-messages="errors.weight_increment"
-            />
+            <v-text-field v-model="config.weight_increment" :label="$t('weight_increment')" />
         </v-col>
         <v-col cols="3">
-            <v-text-field v-model="config.complexity" :label="$t('complexity')" :error-messages="errors.complexity" />
+            <v-text-field v-model="config.complexity" :label="$t('complexity')" />
         </v-col>
         <v-col cols="3">
-            <v-text-field v-model="config.delay" :label="$t('delay')" :error-messages="errors.delay" />
+            <v-text-field v-model="config.delay" :label="$t('delay')" />
         </v-col>
     </v-row>
 </template>
@@ -29,7 +21,6 @@ import { computed } from "vue";
 
 const props = defineProps<{
     modelValue: FixedFactorConfig;
-    errors: Record<string, string>;
 }>();
 
 const emit = defineEmits<{
