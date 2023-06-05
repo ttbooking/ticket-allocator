780e420f902231ace35e32aed6d4942e56df30bb
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-06-05 17:43:10 +0200

    Match query comments

--- src/Support/MatchQuery.php
+++ src/Support/MatchQuery.php
@@ -17,55 +17,68 @@ class MatchQuery
     {
         return DB::query()
 
+        // выборка пар оператор/тикет
         ->select('o.uuid as operator_uuid', 't.uuid as ticket_uuid')
 
         ->fromSub(
 
+            // из подмножества тикетов...
             Ticket::query()
+                // тикет не закреплён либо время резервации вышло
                 ->where(static fn (EloquentBuilder $query) => $query
                     ->whereNull('handler_uuid')
                     ->orWhereNull('accepted_at')
                     ->where('reserved_until', '<=', DB::raw('NOW()'))
                 )
+                // тикет не задержан либо время задержки вышло
                 ->where('delayed_until', '<=', DB::raw('NOW()'))
+                // в порядке убывания веса
                 ->orderByRaw('initial_weight + TIMESTAMPDIFF(SECOND, created_at, NOW()) * weight_increment DESC'),
 
             't'
 
         )->joinSub(
 
+            // ... и операторов
             Operator::query()
+                // оператор онлайн и готов к работе
                 ->where('online', true)
                 ->where('ready', true)
+                // у оператора есть свободные слоты
                 ->where(static fn (EloquentBuilder $query) => $query
                     ->whereNull('free_slots')
                     ->orWhere('free_slots', '>', 0)
                 )
+                // в порядке убывания числа свободных слотов и единиц сложности
                 ->orderByDesc('free_slots')
                 ->orderByDesc('free_complexity'),
 
             'o',
 
-            static fn (JoinClause $join) => $join
-                ->on(static fn (Builder $query) => $query
-                    ->where(static fn (Builder $query) => $query
-                        ->whereNull('o.free_complexity')
-                        ->orWhereColumn('t.complexity', '<=', 'o.free_complexity')
-                    )
-                    ->where(static fn (Builder $query) => $query
-                        ->whereNull('t.handler_uuid')
-                        ->orWhereColumn('t.handler_uuid', '<>', 'o.uuid')
-                    )
-                    //->whereJsonContains('o.matching->categories', DB::raw('json_quote(t.category_uuid)'))
+            // с условием, что...
+            static fn (JoinClause $join) => $join->on(static fn (Builder $query) => $query
+                // свободных единиц сложности достаточно для тикета
+                ->where(static fn (Builder $query) => $query
+                    ->whereNull('o.free_complexity')
+                    ->orWhereColumn('t.complexity', '<=', 'o.free_complexity')
                 )
+                // тикет не закреплён либо закреплён за другим оператором
+                ->where(static fn (Builder $query) => $query
+                    ->whereNull('t.handler_uuid')
+                    ->orWhereColumn('t.handler_uuid', '<>', 'o.uuid')
+                )
+                //->whereJsonContains('o.matching->categories', DB::raw('json_quote(t.category_uuid)'))
+            )
 
         )
 
+        // ... при том, что оператору позволено работать с заданной категорией тикетов
         ->join('ticket_allocator_operator_category as otc', static fn (JoinClause $join) => $join
             ->on('otc.operator_uuid', 'o.uuid')
             ->on('otc.category_uuid', 't.category_uuid')
         )
 
+        // взять первую пару
         ->take(1);
     }
 }
