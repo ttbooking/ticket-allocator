5e784cc154b71a5fbb8b696f7b286f62fc27c024
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-12-27 13:30:09 +0200

    Update dependencies

---
+++ public/build/assets/VContainer-u1vbehWe.js
@@ -0,0 +1,2 @@
+import"./VGrid-P-fm9w4X.js";import{p as n,m as o,a as r,g as l,u as i,j as c,i as m}from"./app-7uE_ZyWH.js";const u=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),d=l()({name:"VContainer",props:u(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>m(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{d as V};
+//# sourceMappingURL=VContainer-u1vbehWe.js.map
