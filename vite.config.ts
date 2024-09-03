9dbf1d41d90df3235081c70822ec6a0395ece52b
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-09-03 23:55:36 +0200

    Compile Assets workflow - another fix

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": "/resources/js",
-            "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
         },
     },
 });
