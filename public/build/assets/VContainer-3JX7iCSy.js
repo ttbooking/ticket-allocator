994677befe07b04cca3ed6ab6cebd40b0aafef2e
daniser <daniser@users.noreply.github.com>
2024-09-04 08:07:59 +0000

    Compile Assets

--- public/build/assets/VContainer-3JX7iCSy.js
+++
@@ -1,2 +0,0 @@
-import"./VGrid-BDTTWTDR.js";import{p as o,m as i,aH as r,a as l,g as m,u,aS as c,j as p,i as d}from"./app-tMHdiTAL.js";const C=o({fluid:{type:Boolean,default:!1},...i(),...r(),...l()},"VContainer"),y=m()({name:"VContainer",props:C(),setup(e,a){let{slots:s}=a;const{rtlClasses:n}=u(),{dimensionStyles:t}=c(e);return p(()=>d(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},n.value,e.class],style:[t.value,e.style]},s)),{}}});export{y as V};
-//# sourceMappingURL=VContainer-3JX7iCSy.js.map
