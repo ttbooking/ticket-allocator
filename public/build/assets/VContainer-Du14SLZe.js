3d5105bb977c4ad35ce75ee49594522f442737a1
daniser <daniser@users.noreply.github.com>
2024-10-24 11:07:22 +0000

    Compile Assets

--- public/build/assets/VContainer-Du14SLZe.js
+++
@@ -1,2 +0,0 @@
-import"./VGrid-BvOCxmeP.js";import{p as o,m as i,ap as r,a as l,g as m,u,aA as c,j as p,i as d}from"./app-DfqJy2iZ.js";const C=o({fluid:{type:Boolean,default:!1},...i(),...r(),...l()},"VContainer"),y=m()({name:"VContainer",props:C(),setup(e,a){let{slots:s}=a;const{rtlClasses:n}=u(),{dimensionStyles:t}=c(e);return p(()=>d(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},n.value,e.class],style:[t.value,e.style]},s)),{}}});export{y as V};
-//# sourceMappingURL=VContainer-Du14SLZe.js.map
