b8a813118ce153d306f844886c6d526a20250e37
daniser <daniser@users.noreply.github.com>
2024-10-25 11:32:16 +0000

    Compile Assets

---
+++ public/build/assets/VContainer-DL1eXz3d.js
@@ -0,0 +1,2 @@
+/* empty css              */import{p as o,m as i,ay as r,a as l,g as m,u,aJ as c,j as p,i as d}from"./app-4RVqqOA3.js";const C=o({fluid:{type:Boolean,default:!1},...i(),...r(),...l()},"VContainer"),V=m()({name:"VContainer",props:C(),setup(e,a){let{slots:s}=a;const{rtlClasses:n}=u(),{dimensionStyles:t}=c(e);return p(()=>d(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},n.value,e.class],style:[t.value,e.style]},s)),{}}});export{V};
+//# sourceMappingURL=VContainer-DL1eXz3d.js.map
