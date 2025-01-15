1abc2588c466d361ba73f71db8a04d7c7b181fed
daniser <5169543+daniser@users.noreply.github.com>
2025-01-15 08:56:08 +0000

    Compile Assets

---
+++ public/build/assets/VContainer-Dm8AJ-CE.js
@@ -0,0 +1,2 @@
+/* empty css              */import{p as o,m as i,az as r,a as l,g as m,u,aK as c,j as p,i as d}from"./app-DBH4OPyQ.js";const C=o({fluid:{type:Boolean,default:!1},...i(),...r(),...l()},"VContainer"),y=m()({name:"VContainer",props:C(),setup(e,a){let{slots:s}=a;const{rtlClasses:n}=u(),{dimensionStyles:t}=c(e);return p(()=>d(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},n.value,e.class],style:[t.value,e.style]},s)),{}}});export{y as V};
+//# sourceMappingURL=VContainer-Dm8AJ-CE.js.map
