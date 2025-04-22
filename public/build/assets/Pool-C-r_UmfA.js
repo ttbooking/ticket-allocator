5c15d89a0f64685ad0536c47a3d155d82ccd085b
daniser <5169543+daniser@users.noreply.github.com>
2025-04-22 10:57:49 +0000

    Compile Assets

--- public/build/assets/Pool-C-r_UmfA.js
+++
@@ -1,2 +0,0 @@
-import{T as r}from"./TransitionGroup-CnbNmL9_.js";import o from"./Ticket-C8iCg4R8.js";import{O as n,Y as a,U as e,X as c,T as i,F as p,a3 as l,a2 as m,a4 as _}from"./app-9Chs2X98.js";const u=n({__name:"Pool",props:{tickets:{}},setup(d){return(s,f)=>(e(),a(m(r),{tag:"td",name:"tickets",class:"@container pt-1"},{default:c(()=>[(e(!0),i(p,null,l(s.tickets,t=>(e(),a(o,{key:t.id,ticket:t,class:"mr-1 mb-1"},null,8,["ticket"]))),128))]),_:1}))}}),B=_(u,[["__scopeId","data-v-e05ee0d4"]]);export{B as default};
-//# sourceMappingURL=Pool-C-r_UmfA.js.map
