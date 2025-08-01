ffcd9213f2e76b549d2caf17daab06f7a9363dce
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-08-01 12:38:55 +0200

    VBtnEx component fix

--- resources/js/components/VBtnEx.vue
+++ resources/js/components/VBtnEx.vue
@@ -1,19 +1,16 @@
 <template>
     <v-btn v-bind="attrs" @click="visitLink">
-        <template v-for="(_, slot) of slots" #[slot]="scope: unknown">
+        <template v-for="(_, slot) of $slots" #[slot]="scope">
             <slot :name="slot" v-bind="scope || {}" />
         </template>
     </v-btn>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { computed, useAttrs } from "vue";
-import { Method } from "@inertiajs/core";
 import { router } from "@inertiajs/vue3";
-import { VBtn } from "vuetify/components";
 
-const props = defineProps<{ method?: Method }>();
-const slots = defineSlots<VBtn["$slots"]>();
+const props = defineProps({ method: String });
 defineOptions({ inheritAttrs: false });
 
 const attrs = computed(() => {
@@ -28,6 +25,6 @@ const attrs = computed(() => {
 });
 
 function visitLink() {
-    attrs.value.tohref && router.visit(attrs.value.tohref as string, { method: props.method });
+    attrs.value.tohref && router.visit(attrs.value.tohref, { method: props.method });
 }
 </script>
